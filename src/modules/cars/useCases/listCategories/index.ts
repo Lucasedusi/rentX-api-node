7819a3bd1a30caabf822d8db76f9d0ff8ac511dd
lucasedusi <lucasmto.16@hotmail.com>
2022-04-18 15:59:51 -0300

    separating repositories

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ListCategoriesController } from "./ListCategoriesControler";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
