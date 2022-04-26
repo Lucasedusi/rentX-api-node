af6a5cbe63f0bf709874fa3b43cdce4bd170c16f
lucasedusi <lucasmto.16@hotmail.com>
2022-04-26 10:15:27 -0300

    refactoring specifications

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,16 +1,11 @@
 import { Router } from "express";
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
-specificationsRoutes.post("/", (request, response) => {
-  return createSpecificationController.handle(request, response);
-});
+const createSpecificationController = new CreateSpecificationController();
 
-// specificationsRoutes.get("/", (request, response) => {
-//   const all = speficicationsRepository.list();
-
-//   return response.json(all);
-// });
+specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
