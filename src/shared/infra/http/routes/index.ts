80954a6a961e5c55deea368efe148ba343bcb113
lucasedusi <lucasmto.16@hotmail.com>
2022-05-05 22:25:18 -0300

    Create RoutesCar index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { autenthicateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -8,6 +9,7 @@ import { usersRoutes } from "./users.routes";
 const router = Router();
 
 router.use(autenthicateRoutes);
+router.use("/cars", carsRoutes);
 router.use("/users", usersRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
