8d41f0fad3239178951b59b0ae3dc05a7420c7cc
lucasedusi <lucasmto.16@hotmail.com>
2022-05-09 16:08:24 -0300

    List cars by: name, brand and category_id

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 
+import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesControler";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensuredAuthenticated";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   "/",
@@ -14,5 +18,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
